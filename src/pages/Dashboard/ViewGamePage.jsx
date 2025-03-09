d507c3f026c912a84594ccacc4f82bb8279dd66b
zsheperdigian <zsheperdigian@gmail.com>
2025-03-09 15:01:31 -0500

    finished styling for viewgame page. updated link to create trivia page. working on logic to pull up user's created games

--- src/pages/Dashboard/ViewGamePage.jsx
+++ src/pages/Dashboard/ViewGamePage.jsx
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { auth, db } from "../../../app/server/api/firebase/firebaseConfig";
 
 export default function ViewGamePage() {
 
     //create edit Games button logic
     const [games, setGames] = useState([]);
+    const [ userId, setUserId ] = useState(null);
+
+
+    //grabs list of games user has created
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            if (user)
+            {
+                console.log("User Authenticated: ", user)
+                setUserId(user.uid);
+
+            } else {
+                console.log("No user Authenticated");
+            }
+
+
+        });
+
+        return () => unsubscribe(); // cleanup subscription
+
+    }, []);
 
 
 
@@ -13,17 +36,21 @@ export default function ViewGamePage() {
 
     return (
 
-        <div className="flex flex-col align-middle">
-            <div className="border border-black">
+        <div className="flex flex-col w-full items-center">
+            <div className=" mt-2 mb-2 pr-4">
                 <h1>Trivia Games</h1>
             </div>
 
             <div>
 
-                <div>
-                    <button type="button" className="border">Create Trivia Game</button>
+                <div className="mt-4 mb-2 border border-black rounded-lg pt-1 bg-black">
+
+                    <Link
+                        className="text-3xl text-green-500  pt-2 pr-2 pl-2 "
+                        to="/dashboard/create-game"> Create Trivia Game
+                    </Link>    
                 </div>
-                        <p>Games you've created</p>
+                <p className="text-xl  border-b-2 border-0 border-black">Games you've created</p>
                 <div>
                     {games.map((game, index) => (
                         <Gamecard />
