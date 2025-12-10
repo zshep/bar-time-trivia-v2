4e044cb962c94b851c33286e25ad6c501116f813
zsheperdigian <zsheperdigian@gmail.com>
2025-12-10 14:05:00 -0600

    filled out playerstats with proper player stats from firestore

--- src/components/dashboard/playerstats.jsx
+++ src/components/dashboard/playerstats.jsx
@@ -1,4 +1,31 @@
-export default function Playerstats(){
+import { useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../../app/server/api/firebase/firebaseConfig";
+
+export default function Playerstats({ userId }){
+    const [stats, setStats] = useState(null);
+
+    //grabbing users stats
+    useEffect(() => {
+    if (!userId) return;
+
+    const ref = doc(db, "users", userId);
+    const unsub = onSnapshot(ref, (snap) => {
+      const data = snap.data();
+      setStats(data?.stats || null);
+    });
+
+    return () => unsub();
+  }, [userId]);
+
+  if (!stats) {
+    return (
+        <div className="p-4 border rounded shadow bg-gray-50">
+        <p className="text-gray-500 italic">No stats yet — play your first game!</p>
+      </div>
+    );
+  }
+  
 
     return (
         <div>
