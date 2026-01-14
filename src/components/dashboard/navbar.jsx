03000691e085d0961187346ed05f3a9a729209db
Shep Sheperdigian <zsheperdigian@gmail.com>
2026-01-14 06:13:05 -0600

    styling for navbar

--- src/components/dashboard/navbar.jsx
+++ src/components/dashboard/navbar.jsx
@@ -5,19 +5,19 @@ export default function Navbar() {
  
 
     return(
-        <div className="flex flex-col justify-between border-black border-2 ml-px px-4 rounded w-44 ">
+        <div className="flex flex-col items-center justify-between border-black border-2 ml-px px-4 rounded w-44 ">
             <div className="mt-4">
                 <ul className="nav flex-column text-center  text-lg">
-                    <li className="nav-item mb-1 border border-black">
+                    <li className="nav-item mb-1 border border-black rounded">
                         <Link className="nav-link active text-black" to="/dashboard">Home</Link>
                     </li>
-                    <li className="nav-item mb-1 border border-black">
+                    <li className="nav-item mb-1 border border-black rounded">
                         <Link className="nav-link text-black" to="viewgamePage">View Trivia Games</Link>
                     </li>
-                    <li className="nav-item mb-1 border border-black">
+                    <li className="nav-item mb-1 border border-black rounded">
                         <Link className="nav-link text-black" to="create-session">Create Trivia Session</Link>
                     </li>
-                    <li className="nav-item mb-1 border border-black">
+                    <li className="nav-item mb-1 border border-black rounded">
                         <Link className="nav-link text-black" to="join-session">Join Trivia Session</Link>
                     </li>
                 
